d025da38ba471336ae94ce55b3813df8b3050cf3
Rasha Rahman <rasharahman@rashas-mbp.lan>
2021-01-10 19:34:46 -0800

    fire

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -4,7 +4,6 @@ import { verifyIdToken } from "../../firebase-admin";
 export default async (_, res) => {
   try {
     const token = await _.query.token;
-
     const tok = await verifyIdToken(token);
     return res.send(tok);
   } catch (err) {
