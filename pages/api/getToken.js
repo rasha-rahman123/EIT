c0158e3d9552ab877111103f95dd94462418db26
Rasha Rahman <rasharahman@rashas-mbp.lan>
2021-01-10 19:36:31 -0800

    the message

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -4,8 +4,11 @@ import { verifyIdToken } from "../../firebase-admin";
 export default async (_, res) => {
   try {
     const token = await _.query.token;
+
     const tok = await verifyIdToken(token);
+
     return res.send(tok);
+    
   } catch (err) {
     return res.send(err);
   }
