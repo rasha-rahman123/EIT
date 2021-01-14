019d5fdf3b687a5a39c199a2e9a0bcbf933cece0
Rasha Rahman <rasharahman@rashas-mbp.lan>
2021-01-13 17:16:50 -0800

    ok

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,27 +2,21 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import Adapters from "next-auth/adapters";
 import prisma from "../../../config/prisma";
-import { Provider } from "next-auth/client";
+
 
 export default (req, res) =>
   NextAuth(req, res, {
     providers: [
-      Providers.GitHub({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      }),
       Providers.Facebook({
         clientId: process.env.FB_CLIENT_ID,
         clientSecret: process.env.FB_CLIENT_SECRET,
+        state: false
       }),
       Providers.Google({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-      Providers.Twitter({
-        clientId: process.env.TWITTER_KEY,
-        clientSecret: process.env.TWITTER_SECRET,
-      }),
+        state: false
+      })
     ],
     callbacks: {
       redirect: async (url, baseUrl) => {
